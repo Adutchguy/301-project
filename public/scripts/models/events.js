aab001d583953e50f52630bfd550a9d6d541c806
Adutchguy <mdm_8@hotmail.com>
2017-05-01 14:59:17 -0700

    added longitude a, latitude and city to the object constructor

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -18,9 +18,9 @@
   events.requestEvents = () => {
     $.get('https://app.ticketmaster.com/discovery/v2/events.json?size=2&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=aPLdF6GC2G6nLNrygytPbkvPzCU7CjGS')
     .then(data => events.all = data._embedded.events)
-    .then(data => events.fetchAll(data)).then(res => console.log(res)), err => console.log(err)
+    .then(data => events.fetchAll(data)), err => console.log(err)
   };
-  
+
   events.requestEvents();
 
   module.events = events;
--- public/scripts/models/eventsConstructor.js
+++ public/scripts/models/eventsConstructor.js
@@ -5,9 +5,11 @@ function Event(data) {
   this.artist = data._embedded.attractions[0].name;
   this.date = data.dates.start.localDate;
   this.time = data.dates.start.localTime;
-  this.address = data._embedded.venues[0].address;
+  this.address = data._embedded.venues[0].address.line1 + ', ' + data._embedded.venues[0].city.name;
   this.description = data.info;
   this.link = data.url;
   this.image = data.images[0].url;
+  this.latitude = data._embedded.venues[0].location.latitude;
+  this.longitude = data._embedded.venues[0].location.longitude;
   events.objects.push(this);
 }
