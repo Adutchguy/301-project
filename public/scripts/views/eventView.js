4fd5927e0f762a76b20645541f6db748916d4481
Adutchguy <mdm_8@hotmail.com>
2017-05-01 16:12:50 -0700

    Misc Changes

--- public/scripts/views/eventView.js
+++ public/scripts/views/eventView.js
@@ -3,21 +3,9 @@
 (function(module) {
   const repoView = {};
 
-  const ui = function() {
-    let $about = $('#about');
-
-    $about.find('ul').empty();
-    $about.show().siblings().hide();
-  };
-
-  const render = Handlebars.compile($('#repo-template').text());
-
   repoView.index = function() {
-    ui();
-
-    $('#about ul').append(
-      repos.with('name').map(render)
-    );
+    const template = Handlebars.compile($('#event-template').text());
+    console.log(template);
   };
 
   module.repoView = repoView;
