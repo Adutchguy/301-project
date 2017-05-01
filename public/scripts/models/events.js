16c58dfb424e9b9f9fb677e5f71ba42864b22d5d
Adutchguy <mdm_8@hotmail.com>
2017-05-01 14:20:40 -0700

    removed some merge conflict duplications

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -20,20 +20,8 @@
     .then(data => events.all = data._embedded.events)
     .then(data => events.fetchAll(data)).then(res => console.log(res)), err => console.log(err)
   };
+  
   events.requestEvents();
-
-  events.venues = [];
-  events.artist = [];
-
-  events.getData = events.all.map(data => data._embedded.venues[0].name, data._embedded.attractions[0].name);
-
-  events.requestEvents = (callback) => {
-    $.get('https://app.ticketmaster.com/discovery/v2/events.json?size=2&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=aPLdF6GC2G6nLNrygytPbkvPzCU7CjGS')
-    .then(data => events.object = data)
-    .then(data => events.all = data._embedded.events)
-    .then(callback), err => console.log(err)
-  };
-  events.requestEvents(events.getData);
 
   module.events = events;
 
