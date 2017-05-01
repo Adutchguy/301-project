6e4db7361803252cea90fdf3ac4e2bc76303d6ea
Adutchguy <mdm_8@hotmail.com>
2017-05-01 12:17:32 -0700

    Made progress towards getting data into a workable form.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('redis-streams')(redis);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = process.env.DATABASE_URL + 'd84fv509f49qbg';
+const conString = process.env.DATABASE_URL + 'events';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
