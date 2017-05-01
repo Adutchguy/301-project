f6e569ebc97db67fc9936d4446a80e86ef0078af
Adutchguy <mdm_8@hotmail.com>
2017-05-01 15:38:04 -0700

    Various adjustments to events.js and eventsConstructor.js

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -9,20 +9,16 @@
 
   events.getData = [];
 
-  events.fetchAll = function(data) {
-    events.all.map(function(data) {
-      let newEvent = new Event(data);
-    });
-  };
+  events.fetchAll = data => events.all.map(function(data){
+    let newEvent = new eventsConstructor.Event(data)
+  });
 
   events.requestEvents = () => {
     $.get('https://app.ticketmaster.com/discovery/v2/events.json?size=2&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=aPLdF6GC2G6nLNrygytPbkvPzCU7CjGS')
     .then(data => events.all = data._embedded.events)
     .then(data => events.fetchAll(data)), err => console.log(err)
   };
-
   events.requestEvents();
 
   module.events = events;
-
 })(window);
