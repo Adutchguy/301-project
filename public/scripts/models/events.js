05d55c6148372374cc58316f8acc56deb6b270f9
Adutchguy <mdm_8@hotmail.com>
2017-05-02 12:05:41 -0700

    Lynn and I pair programming. Working on CSS, removed background temporarily and began setting up our responsive containers

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -15,7 +15,7 @@
 
   events.requestEvents = (callback) => {
     console.log('test 3');
-    $.get('https://app.ticketmaster.com/discovery/v2/events.json?size=2&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=aPLdF6GC2G6nLNrygytPbkvPzCU7CjGS')
+    $.get('https://app.ticketmaster.com/discovery/v2/events.json?size=1&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=aPLdF6GC2G6nLNrygytPbkvPzCU7CjGS')
     .then(data => events.all = data._embedded.events)
     .then(data => {
       console.log(data);
