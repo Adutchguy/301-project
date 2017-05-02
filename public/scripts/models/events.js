b7bdbb90b3cf06911a6173871c11d91c2436a088
janousek77 <janousek77@gmail.com>
2017-05-02 12:09:48 -0700

    added some to database stuff

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -24,5 +24,49 @@
     .then(callback), err => console.log(err)
   };
 
+  events.prototype.insertRecord = function(callback) {
+    $.post(events.objects, {
+        artist: this.artist;
+        venue: this.venue;
+        date: this.date;
+        time: this.time;
+        address: this.address;
+        description: this.description;
+        link: this.link;
+        image: this.image;
+        latitude: this.latitude;
+        longitude: this.longitude;
+        genre: this.genre;
+      })
+    .then(console.log)
+    .then(callback);
+  };
+
+//   events.prototype.deleteRecord = function(callback) {
+//   $.ajax({
+//     url: `/articles/${this.article_id}`,
+//     method: 'DELETE'
+//   })
+//   .then(console.log)
+//   .then(callback);
+// };
+//
+// events.prototype.updateRecord = function(callback) {
+//   $.ajax({
+//     url: `/articles/${this.article_id}`,
+//     method: 'DELETE',
+//     data: {
+//       author: this.author,
+//       authorUrl: this.authorUrl,
+//       body: this.body,
+//       category: this.category,
+//       publishedOn: this.publishedOn,
+//       title: this.title,
+//       author_id: this.author_id
+//     }
+//   })
+//   .then(console.log)
+//   .then(callback);
+// };
   module.events = events;
 })(window);
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ client.on('error', err => console.error(err));
 
 app.use(express.static('./public'));
 
-app.get('/', (request,response) => response.sendFile('index.html', {root:'./public'})
+app.get('/', (request,response) => response.sendFile('index.html', {root:'./public'}))
 
 function proxyTicketmaster(request, response) {
   console.log('Routing Ticketmaster request for', request.params[0]);
@@ -29,4 +29,28 @@ function proxyTicketmaster(request, response) {
 
 app.get('/ticketmaster/*', proxyTicketmaster);
 
+loadDB();
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    events (
+      artist VARCHAR(255) NOT NULL
+      venue VARCHAR(255) NOT NULL
+      date INT
+      time INT
+      address VARCHAR(255) NOT NULL
+      description VARCHAR(255) NOT NULL
+      link VARCHAR(255) NOT NULL
+      image VARCHAR(255) NOT NULL
+      latitude INT
+      longitude INT
+      genre VARCHAR(255) NOT NULL
+      artist VARCHAR(255) NOT NULL,
+      category VARCHAR(255) NOT NULL
+    );`
+  )
+  .catch(console.error);
+}
