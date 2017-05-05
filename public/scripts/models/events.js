4be3607a4d48581856694ff408fda0d9fb7c877d
Adutchguy <mdm_8@hotmail.com>
2017-05-04 17:17:04 -0700

    pushing with heroku changes

--- public/scripts/models/events.js
+++ public/scripts/models/events.js
@@ -13,8 +13,8 @@
   });
 
   events.requestEvents = (callback) => {
-    console.log(process.env.TICKETMASTER_TOKEN);
-    $.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=20&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=${process.env.TICKETMASTER_TOKEN}`)
+    console.log(TICKETMASTER_TOKEN);
+    $.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=20&sort=date,name,asc&city=Seattle&classificationName=Music&apikey=${TICKETMASTER_TOKEN}`)
     .done(data => events.all = data._embedded.events)
     .done(data => {
       events.fetchAll(data);
